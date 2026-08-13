d341ec863c149c4fc8389bd72cf7b61deecc1ba3
ChandraPtama <153147985+ChandraPtama@users.noreply.github.com>
2026-08-14 04:27:59 +0800

    Delete SpeedPlayer-GitHub-Actions directory

--- SpeedPlayer-GitHub-Actions/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if not exist gradlew.bat (
-  echo Gradle Wrapper belum tersedia.
-  echo Install Gradle 8.14+ lalu jalankan: gradle wrapper
-  exit /b 1
-)
-call gradlew.bat build
-pause
